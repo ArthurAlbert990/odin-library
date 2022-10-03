e7c5eee447ddf0fae3729c9381de6322ccc2723d
Arthur <arthur.albert990@gmail.com>
2022-10-03 10:53:08 -0300

    Add start project structure.

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log('js working')
+
+let myLibrary = [];
+
+
+function Book(title,author,pages,alreadyRead){
+    //constructor
+    //"construct a book object"
+    //add object to myLibrary array
+}
+
+function addBookToLibrary(){
+    //pass book to Book constructor function using user input.
+}
+
+function updateBooksDisplay(){
+    //function to update html with myLibrary books.
+}
