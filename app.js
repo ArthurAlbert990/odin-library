625052eb931a6877d25f228f07a09b32971cf71c
Arthur <arthur.albert990@gmail.com>
2022-10-05 13:38:35 -0300

    Update functions.

--- app.js
+++ app.js
@@ -19,27 +19,59 @@ function addBookToLibrary(){
     
     const title = modalFormData.get('ftitle');
     const author= modalFormData.get('fauthor');
-    const pages= modalFormData.get('fpages');
+    const pages= modalFormData.get('fpage');
     const year= modalFormData.get('fyear');
 
     //FormData doesn't obtain checkboxes, diy:
     const alreadyRead= document.querySelector('#checkbox').checked;
-
-
     //pass book to Book constructor function using user input:
     let newBook = new Book(title,author,pages,year,alreadyRead);
     // push to "db":
     myLibrary.push(newBook);
-    console.log(newBook);
-
+    //console.log(newBook);
+    clearModal(modalFormData);
     toggleModal();
 
     return newBook;
-    
+}
+
+function clearModal(formFieldsData){
+    let field;
+    for(let parameter of formFieldsData.entries()){
+        field = document.querySelector(`#${parameter[0]}`)
+        field.value = '';
+    }
 }
 
 function updateBooksDisplay(){
-    //function to update html with myLibrary books.
+    console.log('updateBooksDisplay')
+    //clear table, empty start:
+    tbody.innerHTML=''
+    //insert books from db:
+    let newRow;
+    let newCell;
+    for(let book of myLibrary){
+        newRow = tbody.insertRow(0)//new row at beginning
+        //insert cells for each value of book 
+        for(let value of Object.values(book)){
+            newCell = newRow.insertCell()
+            newCell.innerHTML = value;
+        }
+        //insert buttons at end
+        newCell = newRow.insertCell()
+        newCell.innerHTML = `<button type="button" class="edit-btn">edit</button>
+        <button type="button" class="del-btn">delete</button>`;
+    }
+    //add listener for new buttons:
+    let deleteButtons = document.querySelectorAll('.del-btn');
+    let editButtons = document.querySelectorAll('.edit-btn');
+
+    deleteButtons.forEach(element => {
+        element.addEventListener('click', deleteBook)    
+        });
+    editButtons.forEach(element => {
+        element.addEventListener('click', editBook)
+        });
 }
 
 function deleteBook(e){
@@ -50,6 +82,7 @@ function deleteBook(e){
     //delete tr child element from tbody
     //tr.parentNode = <tbody>
     console.log(`tr index to remove:${tr.rowIndex}`)
+    myLibrary.splice(tr.rowIndex-1,tr.rowIndex-1)
     tr.parentNode.removeChild(tr);
     
 
@@ -64,20 +97,18 @@ function editBook(){
 function toggleModal(){
     console.log('toggle modal')
     modal.classList.toggle('show-modal')
-    //modal to collect user inputs
-    //pass to addBooktToLibrary function
 }
 
-//BINDING
 // selectors
-const modal = document.querySelector('.modal')
-const closeModal = document.querySelector('.close-btn')
-const addBook = document.querySelector('.add-book')
-const confirmButtonModal = document.querySelector('.confirm-btn')
-const deleteButtons = document.querySelectorAll('.del-btn')
-const editButtons = document.querySelectorAll('.edit-btn')
-const form = document.querySelector('.form-addbook')
-
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.close-btn');
+const addBook = document.querySelector('.add-book');
+const confirmButtonModal = document.querySelector('.confirm-btn');
+const deleteButtons = document.querySelectorAll('.del-btn');
+const editButtons = document.querySelectorAll('.edit-btn');
+const form = document.querySelector('.form-addbook');
+const tbody = document.querySelector('.bookDisplay');
+const table = document.querySelector('.books-table')
 
 //bind events
 addBook.addEventListener('click', toggleModal)
@@ -85,17 +116,16 @@ confirmButtonModal.addEventListener('click',addBookToLibrary)
 closeModal.addEventListener('click', toggleModal)
 
 
+
 deleteButtons.forEach(element => {
     element.addEventListener('click', deleteBook)    
-});
-
+    });
 editButtons.forEach(element => {
     element.addEventListener('click', editBook)
-})
+    });
 
 
 //OQUE FALTA IMPLEMENTAR:
-//addBookToLibrary, limpar os campos antes do modal fechar.
 //função deleteBook() ainda não deleta do db
 //função editBook()
-//função updateBooksDisplay()
+
